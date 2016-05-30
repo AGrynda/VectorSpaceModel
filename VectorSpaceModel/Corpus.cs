319fa71eeff58480ad75ae9e8e358995e2cf932a
andriyh <andriyh@compatibl.com>
2016-05-30 21:12:31 +0200

    init

---
+++ VectorSpaceModel/Corpus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorSpaceModel
+{
+    public interface ICorpus
+    {
+
+    }
+
+    public class Corpus : ICorpus
+    {
+        IList<Document> documents;
+        IList<string> terms;
+
+        IList<string> selectedTerms;
+    }
+}
---
+++ VectorSpaceModel/Document.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorSpaceModel
+{
+    class Document
+    {
+        IList<string> terms;
+        IDictionary<string, double> termFrequency;
+
+    }
+}
